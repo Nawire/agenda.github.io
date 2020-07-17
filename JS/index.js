7632a15e3bb3537fd41660f55648d69d3b002760
Nawire <nscovia510@gmail.com>
2020-07-16 23:51:42 -0600

    added my meeting agenda

---
+++ JS/index.js
@@ -0,0 +1,88 @@
+ //get element by id//
+ var y =document.getElementById("myAdr")
+ y.style.color ="blue"; 
+ y.style.fontFamily ="fantasy"
+ y.style.fontSize="36px";
+ y.style.textAlign="center";
+ y.style.fontStyle="italic"
+ 
+ var x = document.getElementById("date&time");
+  x.style.color = "blue";
+  x.style.fontSize ="20px";
+
+
+var agenda =document.getElementById("agenda")
+agenda.style.color ="black";
+agenda.style.fontSize="35px";
+agenda.style.fontFamily="fantasy";
+  
+
+//styling the time of arrival//
+var a =document.getElementById("activity")
+a.style.color ="black";
+a.style.fontStyle ="italic";
+a.style.fontSize="18px";
+
+var act =document.getElementById("act")
+act.style.color ="blue";
+act.style.fontSize="24px";
+act.style.fontFamily="monospace";
+
+//styling the topics//
+var topic1 =document.getElementById("topic1")
+topic1.style.color ="blue";
+topic1.style.fontFamily="monospace";
+topic1.style.fontSize="24px";
+
+var topic2 =document.getElementById("topic2") 
+topic2.style.color ="blue";
+topic2.style.fontFamily="monospace";
+topic2.style.fontSize="24px";
+
+var topic3 =document.getElementById("topic3")
+topic3.style.color ="blue";
+topic3.style.fontFamily="monospace";
+topic3.style.fontSize="24px";
+
+var topic4 = document.getElementById("topic4")
+topic4.style.color ="blue";
+topic4.style.fontFamily="monospace";
+topic4.style.fontSize="24px";
+
+//changing the differennt bullets//
+var b = document.getElementById("bullet")
+b.style.color ="black";
+b.style.fontFamily ="sans-serif";
+b.style.fontSize ="18px";
+b.style.fontStyle ="italic";
+
+var n = document.getElementById("numbers")
+n.style.color ="black";
+n.style.fontFamily ="sans-serif";
+n.style.fontSize ="18px";
+n.style.fontStyle ="italic";
+
+var l = document.getElementById("letters")
+l.style.color ="black";
+l.style.fontFamily ="sans-serif";
+l.style.fontSize ="18px";
+l.style.fontStyle ="italic";
+
+var R = document.getElementById("number")
+R.style.color ="black";
+R.style.fontFamily ="sans-serif";
+R.style.fontSize ="18px";
+R.style.fontStyle ="italic";
+
+//styling the footer elements//
+var p =document.getElementById("pledge")
+p.style.color = "black";
+p.style.fontSize ="25";
+p.style.textAlign = "center";
+
+var f =document.getElementById("footer");
+f.style.color = "black";
+f.style.fontSize ="25";
+f.style.textAlign = "center";
+
+
